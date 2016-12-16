f05c2f8639bb43ced136d450ebea1a0f3cd73bf8
Ryotaro Sumi <ryokirito628@gmail.com>
2016-12-16 11:30:44 +0900

    👼
    先生クラスの追加。
    予約機能っぽいの実装。

--- Sample_TS/TeacherSercher.cs
+++ Sample_TS/TeacherSercher.cs
@@ -6,9 +6,12 @@ namespace TeacherSearcher
 {
 	class TeacherSearcher : Form
 	{
-		private TeacherSwitch ts;
-		private StudentSwitch ss;
-		private bool isExist { get; set; }
+		public TeacherSwitch ts;
+		public Teacher t;
+
+		public Label lb1 = new Label();
+		public Label lb2 = new Label();
+		public Label lb3 = new Label();
 
 		public static void Main()
 		{
@@ -18,51 +21,58 @@ namespace TeacherSearcher
 		public TeacherSearcher()
 		{
 			this.Text = "TeacherSearcher";
-			this.Width = 500;
-			this.Height = 200;
-			this.isExist = false;
+			this.Width = 250;
+			this.Height = 300;
 
-			FlowLayoutPanel flp1 = new FlowLayoutPanel();
-			flp1.Dock = DockStyle.Fill;
+			lb1 = new Label();
+			lb2 = new Label();
+			lb3 = new Label();
 
-			Label lb1 = new Label();
-			lb1.Text = "居室状況";
-			lb1.Parent = flp1;
-
+			FlowLayoutPanel flp = new FlowLayoutPanel();
+			flp.Dock = DockStyle.Fill;
+			flp.FlowDirection = FlowDirection.TopDown;
+			flp.AutoScroll = true;
 
 			// 教授側スイッチ
 			ts = new TeacherSwitch();
-			ts.isExist = this.isExist;
+			t = new Teacher( "fuga" );
+			ts.isExist = t.isExist;
 			ts.RefreshEvent += delegate ( object sender, EventArgs e )
-								{
-									this.isExist = ts.isExist;
-									lb1.Text = (this.isExist) ? "Exist." : "Out.";
-								};
+			{
+				t.isExist = ts.isExist;
+				lb2.Text = (t.isExist) ? "居室状況:Exist" : "居室状況:Out";
+			};
 			ts.Show();
 
-			// 生徒側スイッチ
-			//ss = new StudentSwitch();
-			//ss.Show();
-			FlowLayoutPanel flp2 = new FlowLayoutPanel();
-			flp2.Dock = DockStyle.Bottom;
+			lb1.Text = t.name;
+			lb1.Text += "先生";
+			lb1.Parent = flp;
 
+			lb2.Text = "居室状況:";
+			lb2.Text += t.isExist;
+			lb2.Parent = flp;
+
+			lb3.Text = "面会希望者:";
+			lb3.Text += t.resMan;
+			lb3.Text += "人";
+			lb3.Parent = flp;
+			
 			Button bt1 = new Button();
-			bt1.Text = "up";
+			bt1.Text = "next";
 			bt1.Click += new EventHandler( ClickUpButtonEvent );
-			bt1.Parent = flp2;
+			bt1.Parent = flp;
 
 			Button bt2 = new Button();
-			bt2.Text = "down";
+			bt2.Text = "back";
 			bt2.Click += new EventHandler( ClickDownButtonEvent );
-			bt2.Parent = flp2;
+			bt2.Parent = flp;
 
 			Button bt3 = new Button();
-			bt3.Text = "select";
+			bt3.Text = "reserve";
 			bt3.Click += new EventHandler( ClickSelectButtonEvent );
-			bt3.Parent = flp2;
+			bt3.Parent = flp;
 
-			flp1.Parent = this;
-			flp2.Parent = this;
+			flp.Parent = this;
 		}
 
 		private void ClickUpButtonEvent( Object sender, EventArgs e )
@@ -78,6 +88,11 @@ namespace TeacherSearcher
 		private void ClickSelectButtonEvent( Object sender, EventArgs e )
 		{
 			//
+			t.resMan++;
+
+			lb3.Text = "面会希望者:";
+			lb3.Text += t.resMan;
+			lb3.Text += "人";
 		}
 	}
 }
