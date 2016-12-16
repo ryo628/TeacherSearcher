9ed40a1bbfefb932ab3fd932735943e107e99ae4
Ryotaro Sumi <ryokirito628@gmail.com>
2016-12-16 12:08:32 +0900

    管理方法修正
    先生とスイッチをリストに

--- Sample_TS/TeacherSercher.cs
+++ Sample_TS/TeacherSercher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using TeacherSearcher;
 
@@ -6,12 +7,18 @@ namespace TeacherSearcher
 {
 	class TeacherSearcher : Form
 	{
-		public TeacherSwitch ts;
-		public Teacher t;
+		List<Teacher> teacher;
+		List<TeacherSwitch> tSwitch;
+		public int tNum;
+		public int maxTNum;
 
 		public Label lb1 = new Label();
 		public Label lb2 = new Label();
 		public Label lb3 = new Label();
+		private MainMenu mainMenu1;
+		private System.ComponentModel.IContainer components;
+		private MenuItem menuItem1;
+		private MenuItem menuItem2;
 
 		public static void Main()
 		{
@@ -21,39 +28,48 @@ namespace TeacherSearcher
 		public TeacherSearcher()
 		{
 			this.Text = "TeacherSearcher";
-			this.Width = 250;
-			this.Height = 300;
+			this.Width = 200;
+			this.Height = 250;
+			this.tNum = 0;
+			this.maxTNum = -1;
 
 			lb1 = new Label();
 			lb2 = new Label();
 			lb3 = new Label();
+
+			InitializeComponent();
 
 			FlowLayoutPanel flp = new FlowLayoutPanel();
 			flp.Dock = DockStyle.Fill;
 			flp.FlowDirection = FlowDirection.TopDown;
 			flp.AutoScroll = true;
 
-			// 教授側スイッチ
-			ts = new TeacherSwitch();
-			t = new Teacher( "fuga" );
-			ts.isExist = t.isExist;
-			ts.RefreshEvent += delegate ( object sender, EventArgs e )
+			// スイッチ
+			teacher = new List<Teacher>();
+			tSwitch = new List<TeacherSwitch>();
+			teacher.Add( new Teacher( "fuga" ) );
+			maxTNum++;
+			tSwitch.Add( new TeacherSwitch( teacher[tNum].name ) );
+			//t = new Teacher( "fuga" );
+			//ts = new TeacherSwitch( t.name );
+			tSwitch[tNum].isExist = teacher[tNum].isExist;
+			tSwitch[tNum].RefreshEvent += delegate ( object sender, EventArgs e )
 			{
-				t.isExist = ts.isExist;
-				lb2.Text = (t.isExist) ? "居室状況:Exist" : "居室状況:Out";
+				teacher[tNum].isExist = tSwitch[tNum].isExist;
+				lb2.Text = (teacher[tNum].isExist) ? "居室状況:Exist" : "居室状況:Out";
 			};
-			ts.Show();
+			tSwitch[tNum].Show();
 
-			lb1.Text = t.name;
+			lb1.Text = teacher[tNum].name;
 			lb1.Text += "先生";
 			lb1.Parent = flp;
 
 			lb2.Text = "居室状況:";
-			lb2.Text += t.isExist;
+			lb2.Text += teacher[tNum].isExist;
 			lb2.Parent = flp;
 
 			lb3.Text = "面会希望者:";
-			lb3.Text += t.resMan;
+			lb3.Text += teacher[tNum].resMan;
 			lb3.Text += "人";
 			lb3.Parent = flp;
 			
@@ -78,21 +94,65 @@ namespace TeacherSearcher
 		private void ClickUpButtonEvent( Object sender, EventArgs e )
 		{
 			//
+			tNum++;
+			if( tNum > maxTNum ) tNum = 0;
 		}
 
 		private void ClickDownButtonEvent( Object sender, EventArgs e )
 		{
 			//
+			tNum--;
+			if( tNum < 0 ) tNum = maxTNum;
 		}
 
 		private void ClickSelectButtonEvent( Object sender, EventArgs e )
 		{
 			//
-			t.resMan++;
+			teacher[tNum].resMan++;
 
 			lb3.Text = "面会希望者:";
-			lb3.Text += t.resMan;
+			lb3.Text += teacher[tNum].resMan;
 			lb3.Text += "人";
+		}
+
+		private void addMenuItem_Click( object sender, EventArgs e )
+		{
+			MessageBox.Show( "added" );
+		}
+
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.mainMenu1 = new MainMenu( this.components);
+			this.menuItem1 = new MenuItem();
+			this.menuItem2 = new MenuItem();
+			this.SuspendLayout();
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem2});
+			this.menuItem1.Text = "追加";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 0;
+			this.menuItem2.Text = "先生追加";
+			this.menuItem2.Click += addMenuItem_Click;
+			// 
+			// TeacherSearcher
+			// 
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Menu = this.mainMenu1;
+			this.Name = "TeacherSearcher";
+			this.ResumeLayout(false);
+
 		}
 	}
 }
--- Sample_TS/TeacherSwitch.cs
+++ Sample_TS/TeacherSwitch.cs
@@ -13,11 +13,43 @@ namespace TeacherSearcher
 		public TeacherSwitch()
 		{
 			this.Text = "TacherSwitch";
-			this.Width = 200;
+			this.Width = 75;
 			this.Height = 100;
 
 			FlowLayoutPanel flp = new FlowLayoutPanel();
 			flp.Dock = DockStyle.Fill;
+			flp.FlowDirection = FlowDirection.TopDown;
+			flp.AutoScroll = true;
+
+			Label lb1 = new Label();
+			lb1.Text = "hoge";
+			lb1.Text += "先生";
+			lb1.Parent = flp;
+
+			// button
+			Button bt = new Button();
+			bt.Text = "入室/退出";
+			bt.Click += new EventHandler( ClickEvent );
+			bt.Parent = flp;
+
+			flp.Parent = this;
+		}
+
+		public TeacherSwitch( string name )
+		{
+			this.Text = "TacherSwitch";
+			this.Width = 100;
+			this.Height = 150;
+
+			FlowLayoutPanel flp = new FlowLayoutPanel();
+			flp.Dock = DockStyle.Fill;
+			flp.FlowDirection = FlowDirection.TopDown;
+			flp.AutoScroll = true;
+
+			Label lb1 = new Label();
+			lb1.Text = name;
+			lb1.Text += "先生";
+			lb1.Parent = flp;
 
 			// button
 			Button bt = new Button();
